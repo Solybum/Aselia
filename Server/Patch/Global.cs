6a14312a47b44b31ed132bf503e9367b05dd1119
Soleil Rojas <soleilrojas@outlook.com>
2017-02-08 06:50:10 -0600

    Updated welcome message

--- Server/Patch/Global.cs
+++ Server/Patch/Global.cs
@@ -9,6 +9,7 @@ namespace Patch
     public abstract class Global
     {
         public static readonly string PatchWelcomeMessage = "Patch Server. Copyright SonicTeam, LTD. 2001";
-        public static readonly string WelcomeMessage = "Welcome to a \tC2C#\tC7 port of the \tC6Tethealla\tC7 - \tC3Patch Server\tC7.";
+
+        public static readonly string WelcomeMessage = "Welcome to $C|Aselia$C7's $C3Patch Server$C7";
     }
 }
