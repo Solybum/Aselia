791a0bb825a28c4fe347d2287dacd5f90cd74d50
Soleil Rojas <soleilrojas@outlook.com>
2017-02-13 04:35:01 -0600

    Added summary to assembly resolve. It currently doesn't work in Mono

--- Patch/Patch/Program.cs
+++ Patch/Patch/Program.cs
@@ -6,11 +6,30 @@ namespace Aselia.Patch
 {
     class Program
     {
+        /// <summary>
+        /// Custom assembly resolver so we can store all the libraries 
+        /// in a single folder while still being able to separate 
+        /// each server on it's own directory.
+        /// This method currently does not work in Mono
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static Assembly AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string assemblyPath = Path.Combine("..", "lib", new AssemblyName(args.Name).Name + ".dll");
+            if (!File.Exists(assemblyPath))
+            {
+                return null;
+            }
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
+            return assembly;
+        }
+
         static void Main(string[] args)
         {
-            AppDomain currentDomain = AppDomain.CurrentDomain;
-            currentDomain.AssemblyResolve += new ResolveEventHandler(AssemblyResolver);
-            
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(AssemblyResolve);
+
             string cmdtitle = string.Format("{0} v{1}.{2}",
                 ((AssemblyTitleAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0]).Title,
                 Assembly.GetExecutingAssembly().GetName().Version.Major,
@@ -21,23 +40,9 @@ namespace Aselia.Patch
 
             Server s = new Server();
             s.Start();
-
 #if DEBUG
             Console.Read();
 #endif
         }
-
-        static Assembly AssemblyResolver(object sender, ResolveEventArgs args)
-        {
-            string folderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string assemblyPath = Path.Combine(@"..\lib\", new AssemblyName(args.Name).Name + ".dll");
-            if (!File.Exists(assemblyPath))
-            {
-                return null;
-            }
-            Assembly assembly = Assembly.LoadFrom(assemblyPath);
-            return assembly;
-        }
     }
 }
- 
