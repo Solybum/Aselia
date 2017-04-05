ce38d21b4f637ff2e0082c8fa850dbf5b690825a
Soleil Rojas <soleilrojas@outlook.com>
2017-04-04 22:06:22 -0600

    Removed "standalone" patch server. I have decided to go back to a single solution for all servers and certain tools and libraries. Soon­™ I'll clean the projects and remove some private code to upload them publicly.

--- Patch/Patch/Utils/Global.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Aselia.Patch.Utils
-{
-    public abstract class Global
-    {
-        public static readonly string PatchWelcomeMessage = "Patch Server. Copyright SonicTeam, LTD. 2001";
-    }
-}
